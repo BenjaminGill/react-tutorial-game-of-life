31c236c3cb9ef03f21346fd0c8b059f732236531
benjamin gill <ben_gill@yahoo.com>
2017-03-18 09:44:30 -0400

    adding ability to toggle cell

--- src/BoardRenderer.jsx
+++ src/BoardRenderer.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Alert, Col, Row } from 'react-bootstrap';
+import { toggleCell } from './actions';
 
-const renderRow = row =>
-  row.map((cell, i) => <Col sm={1} key={i} className={cell ? 'cell alive' : 'cell dead'} />);
+class BoardRenderer extends React.Component {
 
-const renderBoard = tiles =>
-  tiles.map((row, index) => <Row key={index}>{renderRow(row)}</Row>);
-
-const BoardRenderer = ({ tiles }) => {
-  try {
-    return <div className="board-renderer">{tiles ? renderBoard(tiles) : ''}</div>;
-  } catch (err) {
-    return <div><Alert bsStyle="danger">{err.message}</Alert></div>;
+  renderCell(cell, rowIndex, colIndex) {
+    return (
+      <div className={cell ? 'cell alive' : 'cell dead'}
+        onClick={this.props.toggleCell.bind(this.props.tiles, rowIndex, colIndex)}
+      />);
   }
-};
+
+  renderRow(row, rowIndex) {
+    return row.map((cell, i) => (
+      <Col md={1} key={i}
+        className={cell ? 'cell alive' : 'cell dead'}
+        onClick={this.props.toggleCell.bind(this.props.tiles, rowIndex, i)}
+      />));
+  }
+
+  renderBoard(tiles) {
+    return tiles.map((row, index) => <Row key={index}>{this.renderRow(row, index)}</Row>);
+  }
+
+  render() {
+    try {
+      return <div className="board-renderer">{this.props.tiles ? this.renderBoard(this.props.tiles) : ''}</div>;
+    } catch (err) {
+      return <div><Alert bsStyle="danger">{err.message}</Alert></div>;
+    }
+  }
+}
 
 const mapStateToProps = state => ({ tiles: state.board.tiles });
-export default connect(mapStateToProps)(BoardRenderer);
+export default connect(mapStateToProps, { toggleCell })(BoardRenderer);
